56697ab45d4fb17e5c607af689601c01dd2df13a
Anish Mangal <anishmg@umich.edu>
2015-01-27 08:23:37 +0000

    Fixing the navbar-brand font (size/color)

--- templates/head.php
+++ templates/head.php
@@ -5,6 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <!--Google font:Lato-->
+  <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 
   <?php wp_head(); ?>
